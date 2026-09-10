b6c8b6195923de4420cb7945279919e892d33f08
威晨 劉 <week861011@gmail.com>
2026-09-10 16:07:02 +0800

    修正無法擴大搜尋

--- prjFriendlyFoodWebAPI/Controllers/FoodMap/PlaceController.cs
+++ prjFriendlyFoodWebAPI/Controllers/FoodMap/PlaceController.cs
@@ -11,7 +11,7 @@ namespace prjFriendlyFoodWebAPI.Controllers.FoodMap
     {
         private readonly IFoodMapService _placeService;
 
-        
+
         public PlaceController(IFoodMapService placeService)
         {
             _placeService = placeService;
@@ -35,17 +35,26 @@ namespace prjFriendlyFoodWebAPI.Controllers.FoodMap
             return Ok(place);
         }
         [HttpGet("nearby")]
-        public async Task<ActionResult<NearbyPlacesDTO>> GetNearbyPlaces(
+        public async Task<ActionResult<List<PlaceDTO>>> GetNearbyPlaces(
             [FromQuery] decimal latitude,
             [FromQuery] decimal longitude,
-            [FromQuery] decimal radiuskm)
+            [FromQuery] decimal radius)
         {
-            var nearbyPlaces = await _placeService.GetNearbyPlacesAsync(new NearbyPlacesDTO
+            var nearbyPlaces = await _placeService.GetNearbyPlacesAsync(new PlacesDTO
             {
                 Latitude = latitude,
                 Longitude = longitude,
-                Radius = radiuskm
+                Radius = radius
             });
+            return Ok(nearbyPlaces);
+        }
+        [HttpGet("nearby/fallback")]
+        public async Task<ActionResult<List<PlaceDTO>>> GetNearbyPlacesWithFallback(
+            [FromQuery] decimal latitude,
+            [FromQuery] decimal longitude)
+        {
+            var nearbyPlaces = await _placeService.GetNearbyPlacesWithFallbackAsync(latitude, longitude);
+
             return Ok(nearbyPlaces);
         }
     }
--- prjFriendlyFoodWebAPI/Services/FoodMap/Interfaces/IFoodMapService.cs
+++ prjFriendlyFoodWebAPI/Services/FoodMap/Interfaces/IFoodMapService.cs
@@ -7,9 +7,9 @@ namespace prjFriendlyFoodWebAPI.Services.FoodMap.Interfaces
         Task<List<PlaceDTO>> GetPlacesAsync();
         Task<PlaceDTO?> GetPlaceByIdAsync(long id);
 
-        Task<List<PlaceDTO>>GetNearbyPlacesAsync(NearbyPlacesDTO request);
+        Task<List<PlaceDTO>>GetNearbyPlacesAsync(PlacesDTO request);
 
-
+        Task<List<PlaceDTO>> GetNearbyPlacesWithFallbackAsync(decimal latitude, decimal longitude);
     }
 
 
